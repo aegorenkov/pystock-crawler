e9e7124277a18782fc7b9aad089bf08bf89bfbc9
Alexander Egorenkov <aegorenk@gmail.com>
2017-05-09 20:52:12 -0400

    REmove levelDB as a requirement by changing scrapy cache settings

--- pystock_crawler/settings.py
+++ pystock_crawler/settings.py
@@ -27,7 +27,7 @@ HTTPCACHE_ENABLED = True
 
 HTTPCACHE_POLICY = 'scrapy.contrib.httpcache.RFC2616Policy'
 
-HTTPCACHE_STORAGE = 'scrapy.contrib.httpcache.LeveldbCacheStorage'
+HTTPCACHE_STORAGE = 'scrapy.contrib.httpcache.FilesystemCacheStorage'
 
 LOG_LEVEL = 'INFO'
 
